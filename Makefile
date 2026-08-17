29f225fbd80f1f05d2853f3cbabff8a9a81e1e9b
VVVietAnh <157110579+HemlockPham7@users.noreply.github.com>
2026-08-17 15:36:38 +0700

    handler unit test

--- Makefile
+++ Makefile
@@ -10,7 +10,7 @@ endif
 export IMG_TAG
 
 COVERAGE_EXCLUDE=mocks|test|level.go|mock.go|request.go|parser.go|postgres_data|infrastructure|migration.go|main.go|config.go|engine.go|worker.go
-COVERAGE_THRESHOLD=70
+COVERAGE_THRESHOLD=90
 COVERAGE_FOLDER=./test-output
 
 docker-test:
--- internal/app/handler/worker/handle_message_test.go
+++ internal/app/handler/worker/handle_message_test.go
@@ -1,1 +1,88 @@
 package worker
+
+import (
+	"context"
+	"encoding/json"
+	"testing"
+
+	mock_bookmark "github.com/HemlockPham7/worker-service/internal/app/service/bookmark/mocks"
+	"github.com/HemlockPham7/worker-service/internal/app/service/queue"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHandler_Handle(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name string
+
+		setupService func(ctx context.Context) *mock_bookmark.Service
+
+		inputMessage []byte
+
+		expected error
+	}{
+		{
+			name: "successfully to handle message",
+
+			setupService: func(ctx context.Context) *mock_bookmark.Service {
+				mockService := mock_bookmark.NewService(t)
+				mockService.On("CreateBatchBookmarks", ctx, "user-123", setupImportBookmarkInput()).Return(nil)
+				return mockService
+			},
+
+			inputMessage: setupInputMessage(),
+
+			expected: nil,
+		},
+		{
+			name: "There is an error",
+
+			setupService: func(ctx context.Context) *mock_bookmark.Service {
+				mockService := mock_bookmark.NewService(t)
+				mockService.On("CreateBatchBookmarks", ctx, "user-123", setupImportBookmarkInput()).Return(assert.AnError)
+				return mockService
+			},
+
+			inputMessage: setupInputMessage(),
+
+			expected: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			ctx := t.Context()
+
+			mockService := tc.setupService(ctx)
+			mockHandler := NewHandler(mockService)
+
+			err := mockHandler.Handle(ctx, tc.inputMessage)
+
+			assert.Equal(t, tc.expected, err)
+		})
+	}
+}
+
+func setupInputMessage() []byte {
+	bookmarkList := setupImportBookmarkInput()
+	input := &queue.ImportMessage{
+		UID:       "user-123",
+		Bookmarks: bookmarkList,
+	}
+	message, err := json.Marshal(input)
+	if err != nil {
+		return []byte("{}")
+	}
+	return message
+}
+
+func setupImportBookmarkInput() []*queue.ImportBookmarkInput {
+	return []*queue.ImportBookmarkInput{
+		{
+			Description: "Bookmark Example",
+			URL:         "https://www.example.com",
+		},
+	}
+}
